2e33f05207bb3cf4910d07dd3ac94f49942213df
vagrant <vagrant@localhost.localdomain>
2020-03-26 07:46:45 +0000

    Add templete

---
+++ main.js
@@ -0,0 +1,22 @@
+//サーバ処理
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
+
+//ルート
+app.get('/' , function(req, res){
+    res.sendFile(__dirname+'/index.html');
+});
+
+//サーバ監視
+http.listen(PORT, function(){
+    console.log('server listening. Port:' + PORT);
+});
+
+
+//socket
+io.on('connection',function(socket){
+
+});
