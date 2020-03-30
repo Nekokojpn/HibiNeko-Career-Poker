8cc4fbd4fd600ceffcf1dd0138995ebde8a4a123
Nekokojpn <nekokojpn@gmail.com>
2020-03-30 18:04:46 +0000

    Add some

--- public/js/ClientNekoCareerPoker.js
+++ public/js/ClientNekoCareerPoker.js
@@ -1,16 +1,26 @@
 let undefined;
+let stauts = [
+    [],
+];
 class ClientNekoCareerPoker {
+    constructor() {
+        this.statuses = new Array();
+    }
     setCurrentTrump(lists) {
         this.curtrump = lists;
     }
     setSubmits(lists) {
         this.submits = lists;
     }
+    updateSubmitStatus() {
+        console.log("Called Update submits!");
+    }
     updateSubmittable() {
         if(this.submits === undefined) {
             console.error("Call setSubmits and then call updateSubmittable!");
             return;
         }
+        updateSubmitStatus();
         if(this.submits.length === 0) {
             for(let i = 0, l = this.curtrump.length; i < l; i++)
                 this.curtrump[i][2] = true;
